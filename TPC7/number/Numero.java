4409c42d52f4051eaedd76599c46bbaa8949044e
machadoatz <franciscocmachado@gmail.com>
2017-11-01 14:51:16 +0000

    SAUCHICHA

--- TPC7/number/Numero.java
+++ TPC7/number/Numero.java
@@ -1,71 +1,84 @@
 package number;
+
 /**
- * This class represents a number. <p>
+ * Esta classe representa um Numero inteiro. <p>
+ * É possivel saber o valor representado pelo número bem como alterá-lo. <p>
+ * As funcionalidades desta classe ainda permitem representar um número numa
+ * String e comparar dois números. <p>
  *
- * @author Francisco Machado
- * @author Bernardo Valente
+ * @author Bernardo Valente & Francisco Machado
  * @version 1.0
  */
 
-public class Numero {
+public class Numero{
 
-	/**
-	* Guarda o valor do numero
+	/** int_num corresponde ao valor inteiro de Numero.
 	*/
-	private int _valor;
+	private int _num;
 
-	  /**
-	  *	Construtor que nao recebe argumentos e inicia o numero a 0
-	  */
-	  public Numero() {
-	  	_valor = 0;
-	  }
+	/** Construtor de um Número
+	*
+	*  @param num é o valor atribuido ao numero.
+	*/
+	public Numero(int num){
+		_num = num;
+	}
+	/** Cria uma instancia que inicialmente representa o valor 0.
+	*/
+	public Numero(){
+		_num = 0;
+	}
 
-	  /**
-	  *	Construtor que recebe como argumento um valor e inicia o numero com esse valor
-	  */
-	  public Numero(int x){
-	  	_valor = x;
-	  }
+	/** Constructor de um Numero
+	*
+	*  @param x._num é o valor atribuido ao numero.
+	*/
+	public Numero(Numero x){
+		_num = x._num;
+	}
 
-	  /**
-	  *	Altera o valor do numero pelo valor que recebe como argumento
-	  */
-	  public void alteraValor(int n) {
-	    _valor = n;
-	  }
+	//main
+	public static void main(String [] argv){
+		System.out.println("Hello World!");
+	}
 
-	  /**
-	  *	Retorna o valor do numero
-	  */
-	  public int obtemValor() {
-	    return _valor;
-	  }
+	/** Metodo retorna o valor inteiro de um Numero.
+	* A este metodo não são passados quaisquer argumentos.
+	*/
+	public int getNum(){
+		return _num;
+	}
+	/** Metodo altera o valor de um Numero.
+	* É passado como argumento um inteiro, este valor será atribuido ao Numero.
+	*
+	* @param newNum valor atribuido ao Numero.
+	*/
+	public void changeNum(int newNum){
+		_num = newNum;
+	}
 
-	  /**
-	  *	Retorna o numero no formato de String
-	  */
-	  public String toString() {
-	    return "" + _valor;
-	  }
+	/** Metodo altera o valor de um Numero.
+	* É passado como argumento um Numero com um valor que é atribuido ao Numero.
+	*
+	* @param newNum Numero com um valor que é atribuido ao Numero.
+	*/
+	public void changeNum(Numero newNum){
+		_num = newNum._num;
+	}
 
-	  /**
-	  *	Recebe um numero e compara-o com o valor do numero da propria class, retornando um boolean
-	  */
-	  public boolean eIgual(Numero n) {
-	    return _valor == n.obtemValor();
-	  }
+	/** Devolve o número representado por uma dada instância como uma String.
+	*
+	*/
+	public void printNum(){
+		System.out.println( _num );
+	}
 
-	  public static void main(String[] args) {
-	    Numero n1 = new Numero();
-	    Numero n2 = new Numero();
-
-	    System.out.println("n1.valor = " + n1.obtemValor());
-	    System.out.println("n1 == n2 -> " + n1.eIgual(n2));
-
-	    n2.alteraValor(4);
-
-	    System.out.println("n1 = " + n1 + " n2 = " + n2);
-	    System.out.println("n1 == n2 -> " + n1.eIgual(n2));
-	  }
+	/** Metodo compara duas instancias de Numero.
+	* Retorna um valor booleano.
+	*
+	* @param x Numero a comparar.
+	*/
+	public boolean compareNum(Numero x){
+		return _num == x.getNum();
+	}
 }
--- TPC7/number/NumeroComMemoria.java
+++ TPC7/number/NumeroComMemoria.java
@@ -1,6 +1,19 @@
 package number;
 
-public class NumeroComMemoria extends Numero {
+/**
+ * Esta classe representa um Numero Com Memoria. <p>
+ *
+ * Inclui todas as funcionalidades de Numero.  <p>
+ * É possivel desfazer a ultima alteração realizada. Se utilizarmos esta funcionalidade duas vezes o numero fica inalterado.
+ *
+ * Caso não exista valor anterior é devolvido o próprio valor do número.
+ *
+ * @author Bernardo Valente & Francisco Machado
+ * @version 1.0
+ */
+
+public class NumeroComMemoria extends Numero{
+	
 	private int _valorAnterior;
 
 	public void alteraValor(int n) {
@@ -16,16 +29,16 @@ public class NumeroComMemoria extends Numero {
 		return _valorAnterior;
 	}
 
-  	public static void main(String[] args) {
-    	NumeroComMemoria n1 = new NumeroComMemoria();
+	public static void main(String[] args) {
+		NumeroComMemoria n1 = new NumeroComMemoria();
 
-    	n1.alteraValor(4);
-    	System.out.println("n1 = " + n1 );
+		n1.alteraValor(4);
+		System.out.println("n1 = " + n1 );
 
-       	n1.desfazer();
-    	System.out.println("n1 = " + n1 );
+		n1.desfazer();
+		System.out.println("n1 = " + n1 );
 
-        n1.desfazer();
-    	System.out.println("n1 = " + n1 );
-  	}
-}
+		n1.desfazer();
+		System.out.println("n1 = " + n1 );
+	}
+	}
