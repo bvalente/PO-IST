14273d9beceb969df67cfc8bfe1fa03cc236a5cd
machadoatz <franciscocmachado@gmail.com>
2017-11-30 11:37:51 +0000

    done

--- lab11/editor/core/Grupo.java
+++ lab11/editor/core/Grupo.java
@@ -1,9 +1,43 @@
 package editor.core;
 
-class Grupo {
+import java.util.List;
+import java.util.ArrayList;
+
+public class Grupo extends Form{
+
+    List<Form> lista ;
+    public Grupo(){
+        super(0,0);
+        lista = new ArrayList<Form>();
+    }
+    public Grupo(int x, int y){
+        this();
+
+    }
+
+    public Grupo(Editor e, int x, int y){
+        this();
+    }
 
 
+    void addToList(Form f){
+        lista.add(f);
+    }
 
+    public String draw(){
+        String str = new String();
+        for ( Form f : lista){
+            str.concat( f.draw() );
+        }
 
+        return str;
+    }
+
+    public void move(int x, int y){
+
+        for ( Form f : lista){
+             f.move(x,y);
+        }
+    }
 
 }
--- lab11/editor/core/SubGrupo.java
+++
@@ -1,9 +0,0 @@
-package editor.core;
-
-class SubGrupo {
-
-
-
-
-
-}
